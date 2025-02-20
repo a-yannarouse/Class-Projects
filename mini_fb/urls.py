f06317c739802707c824343fa14fd85b2d483e70
a-yannarouse <yanni620@bu.edu>
2025-02-20 11:08:00 -0500

    updated mini_fb models and stuff

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,7 +1,9 @@
 # mini_fb/urls.py
 
 from django.urls import path
+from .views import ShowAllProfilesView
+from . import views
 
 urlpatterns = [
-
+    path('', ShowAllProfilesView.as_view(), name="show_all_profiles"),
 ]
